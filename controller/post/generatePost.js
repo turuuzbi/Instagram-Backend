2c08eabada94e6adbd855a850a1975eb78fb7b5d
25HP1499@nest.edu.mn <25HP1499nest.edu.mn@admins-iMac.local>
2025-10-23 18:24:38 +0800

    basically done imo

--- controller/post/generatePost.js
+++ controller/post/generatePost.js
@@ -1,16 +1,12 @@
 import { postModel } from "../../schema/post.Schema.js";
 
 export const generatePost = async (request, response) => {
-  const data = request.body;
   const user = request.user;
-
-  const { images, caption } = data;
-
+  const { images, caption } = request.body;
   const newPost = await postModel.create({
     user: user._id,
     images,
     caption,
   });
-
   response.status(200).json({ newPost });
 };
